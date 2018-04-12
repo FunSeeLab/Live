8c47311578f1fa3c65bbe9eb813dac5b1920ff82
MeatPieYan <robinyzg@hotmail.com>
2018-04-13 00:25:59 +0800

    initial version

--- server/app.js
+++ server/app.js
@@ -1,10 +1,55 @@
 const Koa = require('koa');
+const http = require('http');
+const socket = require('socket.io');
+const views = require('koa-views');
+const path = require('path');
 
 const app = new Koa();
+const server = http.createServer(app.callback());
+const io = socket(server);
+
+let loginUser = [];
+
+io.on('connection', function(socket){
+  let username = '';
+  socket.on('login', (msg) => {
+    username = msg;
+    loginUser.push({ username, socket });
+    console.log('loginUser:', loginUser.join(','));
+  });
+
+  socket.on('logout', (msg) => {
+    console.log(`${username} logout`);
+    loginUser = loginUser.filter((user) => user.name === username);
+    console.log('loginUser:', loginUser.join(','));
+  });
+
+  socket.on('chat', (msg) => {
+    const arr = msg.split('_');
+    if(arr[0].trim() === '') {
+      io.emit('msgHistory', `${username}对大家说：${arr[1]}`);
+      return;
+    }
+    io.emit('msgHistory', `${username}对${arr[0]}说：${arr[1]}`);
+    
+  });
+  
+  socket.on('disconnect', () => {
+    console.log(`${username} disconnected`);
+    loginUser = loginUser.filter((user) => user.name === username);
+    console.log('loginUser:', loginUser.join(','));
+  });
+});
+
+app.use(views(path.resolve(__dirname, '../views'), { map: { html: 'ejs' } }));
 
 app.use(async(ctx, next) => {
-  ctx.body = 'hello world';
-  ctx.status = 200;
+  if(ctx.request.path === '/') {
+    await ctx.render('index');
+    return;
+  }
+
+  return next();
 })
 
-app.listen(3000)
+server.listen(3000);
